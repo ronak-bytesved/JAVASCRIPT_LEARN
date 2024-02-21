0f77a95e718ce11b16ad4f11029dca4f5383ec2f
ronak-bytesved <ronak.bytesved@gmail.com>
2024-02-21 18:11:27 +0530

    add files

---
+++ while_loop.js
@@ -0,0 +1,23 @@
+// while loop is executed if the specified condition is true
+// let p =1;
+// while(p<=10){
+//     console.log("hello world " + p);
+//     p++;
+// }
+
+// do while loop
+
+let  p = ["ronak","tejash","yes","hello","you"];
+let i = 0;
+let text = ""
+while(p[i]){
+    text += p[i] + "\n";
+    i++;
+}
+console.log(text + " ");
+
+
+// do{
+//     console.log("print hello world");
+//     p++;
+// }while(p<=10);
